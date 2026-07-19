afbd2d45faccd52db93c488c204e5a102886a433
Stelare-Rose@Snowflake <stelare-rose@noreply.git.starrytea.cc>
2026-07-19 14:51:56 +0700

    Feat: Added Stop Function

--- src/actions.rs
+++ src/actions.rs
@@ -3,7 +3,7 @@ use std::{fs, path::PathBuf};
 use chrono::{DateTime, Utc};
 use constellation_eridanus::{Eri, serialize};
 use sanitize_filename::sanitize;
-use uuid::Uuid;
+use anyhow::anyhow;
 
 use crate::{types::{Event, EventId, ProjectId, TagId}, utils::{event_path, uuidv7_from_datetime}};
 
@@ -12,6 +12,9 @@ pub struct Horologium { base_path: PathBuf, device_id: String }
 impl Horologium {
     pub fn start(&self, name: String, tags: Vec<TagId>, project: Option<ProjectId>, body: Option<String>, start_time: Option<DateTime<Utc>>) -> anyhow::Result<EventId> {
         start(&self.base_path, &self.device_id, name, tags, project, body, start_time)
+    }
+    pub fn stop(&self, name: Option<String>, body: Option<String> start_time: Option<DateTime<Utc>>) -> anyhow::Result<EventId> {
+        stop(&self.base_path, &self.device_id, name, body, start_time)
     }
 }
 
@@ -31,7 +34,7 @@ pub fn start(
 
     let eri: Eri = e.into();
     // TODO: Change Eri to use actual error types
-    let serialized: String = serialize(&eri).map_err(|e: String| anyhow::anyhow!(e))?;
+    let serialized: String = serialize(&eri).map_err(|e: String| anyhow!(e))?;
 
     // File Write
     let path = event_path(base_path, s);
@@ -46,6 +49,38 @@ pub fn start(
     let file_path = path.join(format!("{sanitized}-{id}.eri"));
     fs::write(file_path, serialized)?;
     
+
+    Ok(EventId(id))
+}
+
+pub fn stop (
+    base_path: &PathBuf,
+    device_id: &str,
+    name: Option<String>,
+    body: Option<String>,
+    start_time: Option<DateTime<Utc>>,
+) -> anyhow::Result<EventId> { 
+    let s = start_time.unwrap_or_else(Utc::now);
+    let id = uuidv7_from_datetime(s).to_string();
+
+    let e = Event::Inactive { id: EventId(id.clone()), name: name.clone(), start: s, body };
+
+    let eri: Eri = e.into();
+    let serialized: String = serialize(&eri).map_err(|e: String| anyhow!(e))?;
+
+    // File Write
+    let path = event_path(base_path, s);
+    fs::create_dir_all(&path)?;
+
+    let clock_path = path.join(format!("{device_id}-clock.txt"));
+    let current: u64 = fs::read_to_string(&clock_path).ok().and_then(|s| s.trim().parse().ok()).unwrap_or(0);
+
+    fs::write(clock_path, (current + 1).to_string())?;
+
+    let sanitized = sanitize(name.unwrap_or("untracked".to_string()));
+    let file_path = path.join(format!("{sanitized}-{id}.eri"));
+
+    fs::write(file_path, serialized)?;
 
     Ok(EventId(id))
 }

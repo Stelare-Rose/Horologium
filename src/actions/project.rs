c8b08d22aed258f0aa4c6ff9c5ea9d44693ead30
Stelare-Rose@Snowdrop <stelare-rose@noreply.git.starrytea.cc>
2026-08-03 10:50:17 +0700

    Feat: Added Project Modification

--- src/actions/project.rs
+++ src/actions/project.rs
@@ -5,7 +5,7 @@ use sanitize_filename::sanitize;
 use anyhow::anyhow;
 use nanoid::nanoid;
 
-use crate::types::{Color, Project, ProjectId, ProjectState};
+use crate::{types::{Color, Project, ProjectId, ProjectState}, utils::event_path};
 
 pub fn define_project(
     base_path: &PathBuf,
@@ -45,5 +45,27 @@ pub fn modify_project(
     from: &PathBuf,
     project: Project
 ) -> anyhow::Result<ProjectId> {
-    todo!()
+    let path = base_path.join("Projects");
+    let sanitized = sanitize(&project.name);
+
+    let sanitized_device = sanitize(device_id);
+    let id = project.id.clone();
+    let name = project.name.clone();
+
+    let e: Eri = project.into();
+    let serialized = serialize(&e).map_err(|e: String| anyhow!(e))?;
+
+    let file_path = path.join(format!("{sanitized}-{id}-{sanitized_device}.eri"));
+    let clock_path = path.join(format!("{sanitized_device}-clock.txt"));
+    let current: u64 = fs::read_to_string(&clock_path).ok().and_then(|s| s.trim().parse().ok()).unwrap_or(0);
+
+    fs::write(clock_path, (current + 1).to_string())?;
+
+    fs::write(&file_path, serialized)?;
+
+    if file_path != *from {
+        fs::remove_file(from)?;
+    };
+
+    Ok(ProjectId { id, name })
 }

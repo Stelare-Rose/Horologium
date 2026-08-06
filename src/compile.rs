3d56c59d5a2f1325168f7261c3d8a4c61bb8a8e3
Stelare-Rose@Snowdrop <stelare-rose@noreply.git.starrytea.cc>
2026-08-06 22:47:39 +0700

    Feat: Added Skeleton for Compile Record Directory

--- src/compile.rs
+++ src/compile.rs
@@ -1,7 +1,20 @@
 use std::path::PathBuf;
 
-pub struct Compile { pub base_path: PathBuf }
+use chrono::NaiveDate;
+
+use crate::{database::Database, utils::event_path};
+
+pub struct Compile { pub base_path: PathBuf, pub database: Database }
 
 impl Compile {
 
+}
+
+pub fn compile_record(
+    base_path: &PathBuf,
+    database: &Database,
+    day: &NaiveDate,
+) -> anyhow::Result<()> {
+    let path = event_path(base_path, day);
+    todo!()
 }

a0e22f66a15100297cef90bcdaeb0d044fd42b29
Stelare-Rose@Snowflake <stelare-rose@noreply.git.starrytea.cc>
2026-08-09 15:41:42 +0700

    Refactor: Moved Common Compile methods to Compile/Utils

--- src/compile.rs
+++ src/compile.rs
@@ -2,22 +2,14 @@ use std::{collections::HashMap, fs, path::{PathBuf}};
 
 use chrono::NaiveDate;
 
-use crate::{database::Database, utils::{event_path, fingerprint}};
+use crate::{compile::utils::{FileActions, compare_fingerprints}, database::Database, utils::{event_path, fingerprint}};
+
+pub mod utils;
 
 pub struct Compile { pub base_path: PathBuf, pub database: Database }
 
 impl Compile {
 
-}
-
-enum FileActions {
-    Upsert {
-        path: PathBuf,
-        fingerprint: u64
-    },
-    Delete {
-        path: PathBuf
-    }
 }
 
 pub fn compile_record(
@@ -33,33 +25,23 @@ pub fn compile_record(
         let fp = fingerprint(&path)?;
         fingerprints.insert(path.canonicalize()?, fp);
     }
-    let mut cache_fingerprints: HashMap<PathBuf, u64> = database.get_fingerprints(&path.canonicalize()?)?;
+    let cache_fingerprints: HashMap<PathBuf, u64> = database.get_fingerprints(&path.canonicalize()?)?;
+    let actions = compare_fingerprints(fingerprints, cache_fingerprints);
+    process_records(actions)?;
 
     // Compare all fingerprints from sqlite and the directory, dropping entries where fingerprints are identical
-    let mut actions: Vec<FileActions> = Vec::new();
-    for (path, fp) in fingerprints {
-        match cache_fingerprints.remove(&path) {
-            Some(cache_fp) if cache_fp != fp => {
-                actions.push(FileActions::Upsert { path, fingerprint: fp });
-            },
-            Some(_) => {
-                // fingerprints are the same, we skip it here
-            }
-            None => { 
-                actions.push(FileActions::Upsert { path, fingerprint: fp });
-            }
-        }
-    }
 
-    // Anything remaining in cache_fingerprints are deletes.
-    for path in cache_fingerprints.into_keys() {
-        actions.push(FileActions::Delete { path });
-    }
 
     // Read and reindex items that aren't identical
     // Remove all remaining entries, remembering to stitch together the enries that were in between
 
     // It's like removing an item from a linked list.
     
+    Ok(())
+}
+
+fn process_records(
+    actions: Vec<FileActions>
+) -> anyhow::Result<()> {
     todo!()
 }
---
+++ src/compile/utils.rs
@@ -0,0 +1,38 @@
+use std::{collections::HashMap, path::PathBuf};
+
+pub enum FileActions {
+    Upsert {
+        path: PathBuf,
+        fingerprint: u64
+    },
+    Delete {
+        path: PathBuf
+    }
+}
+
+pub fn compare_fingerprints(
+    fp: HashMap<PathBuf, u64>,
+    mut c_fp: HashMap<PathBuf, u64>
+) -> Vec<FileActions> {
+    let mut actions: Vec<FileActions> = Vec::new();
+    for (path, fp) in fp {
+        match c_fp.remove(&path) {
+            Some(cache_fp) if cache_fp != fp => {
+                actions.push(FileActions::Upsert { path, fingerprint: fp });
+            },
+            Some(_) => {
+                // fingerprints are the same, we skip it here
+            }
+            None => { 
+                actions.push(FileActions::Upsert { path, fingerprint: fp });
+            }
+        }
+    }
+
+    // Anything remaining in cache_fingerprints are deletes.
+    for path in c_fp.into_keys() {
+        actions.push(FileActions::Delete { path });
+    }
+    actions
+}
+
